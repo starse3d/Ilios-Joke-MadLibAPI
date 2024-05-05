015e93b47c03d617aca0c54eace26c72b9ef7992
starse3d <90733889+starse3d@users.noreply.github.com>
2024-05-05 14:08:17 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/joke", JokeHandler)
+	http.HandleFunc("/madLib", MadLibHandler)
+	fmt.Println("Listening on localhost:8080")
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe", err)
+	}
+
+}
+
+func JokeHandler(w http.ResponseWriter, r *http.Request) {
+	joke := GetRandomJoke()
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte(joke))
+}
+
+func MadLibHandler(w http.ResponseWriter, r *http.Request) {
+	madLib := GetMadLib()
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte(madLib))
+}
